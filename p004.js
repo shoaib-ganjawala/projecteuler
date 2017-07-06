9e3acdf4ba5032fd4ae3beb169424a3485680c7c
shoaib-ganjawala <soyeb@visasoftheworld.com>
2017-07-06 11:03:04 +0530

    dev branch

--- p004.js
+++ p004.js
@@ -1,9 +1,10 @@
 var arr = [];
+
 function isPalindrome(str, mul) {
 	return str.split("").reverse().join("");
 }
 
-function activate(){
+function activate() {
 	for(var i=999; i>=0; i--) {
 		for(var j=999; j>=0; j--) {
 			if((i*j).toString() == isPalindrome((i*j).toString())) {
